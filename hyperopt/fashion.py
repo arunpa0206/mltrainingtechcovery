c86b0578e2e8e2c93596fd3dd8d8b22c9350df90
arunpa0206 <mail_arunn@yahoo.com>
2020-05-24 17:36:46 +0530

    added hyopt for neural networks

---
+++ hyperopt/fashion.py
@@ -0,0 +1,74 @@
+# Import Statements
+
+from hyperopt import Trials, STATUS_OK, tpe
+from hyperas import optim
+from hyperas.distributions import choice, uniform
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD, Adam, RMSprop
+from tensorflow.keras.models import Sequential
+from sklearn.model_selection import train_test_split
+import numpy as np
+from hyperas import optim
+from hyperas.distributions import choice, uniform
+
+#Pre process Data
+def data():
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    X_train, X_val, y_train, y_val = train_test_split(X_train,    y_train, test_size=0.2, random_state=12345)
+    X_train = X_train.reshape(48000, 784)
+    X_val = X_val.reshape(12000, 784)
+    X_train = X_train.astype('float32')
+    X_val = X_val.astype('float32')
+    X_train /= 255
+    X_val /= 255
+    nb_classes = 10
+    Y_train = keras.utils.to_categorical(y_train, nb_classes)
+    Y_val = keras.utils.to_categorical(y_val, nb_classes)
+    return X_train, Y_train, X_val, Y_val
+#
+def model(X_train, Y_train, X_val, Y_val):
+
+    model = Sequential()
+    # choose between any hyper parameter
+    model.add(Dense({{choice([128, 256, 512, 1024])}}, input_shape=(784,)))
+    model.add(Activation({{choice(['relu', 'sigmoid'])}}))
+    model.add(Dropout({{uniform(0, 1)}}))
+    model.add(Dense({{choice([128, 256, 512, 1024])}}))
+    model.add(Activation({{choice(['relu', 'sigmoid'])}}))
+    model.add(Dropout({{uniform(0, 1)}}))
+
+
+
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+    adam = keras.optimizers.Adam(lr={{choice([10**-3, 10**-2, 10**-1])}})
+    rmsprop = keras.optimizers.RMSprop(lr={{choice([10**-3, 10**-2, 10**-1])}})
+    sgd = keras.optimizers.SGD(lr={{choice([10**-3, 10**-2, 10**-1])}})
+
+    choiceval = {{choice(['adam', 'sgd', 'rmsprop'])}}
+    if choiceval == 'adam':
+        optim = adam
+    elif choiceval == 'rmsprop':
+        optim = rmsprop
+    else:
+        optim = sgd
+
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'],optimizer=optim)
+    model.fit(X_train, Y_train,
+              batch_size={{choice([128,256,512])}},
+              nb_epoch=20,
+              verbose=2,
+              validation_data=(X_val, Y_val))
+    score, acc = model.evaluate(X_val, Y_val, verbose=0)
+    print('Test accuracy:', acc)
+    return {'loss': -acc, 'status': STATUS_OK, 'model': model}
+
+X_train, Y_train, X_val, Y_val = data()
+best_run, best_model = optim.minimize(model=model,
+                                      data=data,
+                                      algo=tpe.suggest,
+                                      max_evals=30,
+                                      trials=Trials(),
+                                      )
